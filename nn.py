0f29b60685aec00a0242b93c7f814cf62a0af562
jaschahuisman <jascha.huisman@gmail.com>
2020-05-14 12:02:25 +0200

    Added first initialization to NeuralNetwork

--- nn.py
+++ nn.py
@@ -1,12 +1,16 @@
-class NeuralNetwork():
+from matrix import Matrix
+
+class NeuralNetwork:
     # Initialization
-    def __init__ (self, input_nodes, hidden_nodes, output_nodes):
+    def __init__ (self, input_nodes:int, hidden_nodes:int, output_nodes:int):
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
-    
-    # Test
-    def test(self):
-        print(self.input_nodes)
-        print(self.hidden_nodes)
-        print(self.output_nodes)
+        
+        self.weights_ih = Matrix(self.hidden_nodes, self.input_nodes)
+        self.weights_ho = Matrix(self.output_nodes, self.hidden_nodes)
+        self.weights_ih.randomize()
+        self.weights_ho.randomize()
+
+        self.bias_h = Matrix(self.hidden_nodes, 1)
+        self.bias_o = Matrix(self.hidden_nodes, 1)
