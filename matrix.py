e825f6fe22983a2f1611f03eea38bb08c0127c5f
jaschahuisman <jascha.huisman@gmail.com>
2020-05-15 11:10:46 +0200

    Update matrix.py and nn.py
    Added docstrings in matrix.py, added train function in nn.py

--- matrix.py
+++ matrix.py
@@ -26,12 +26,29 @@ class Matrix:
         
         for i in range(self.rows):
             self.data.append([])
-            for j in range(self.cols):
+            for _ in range(self.cols):
                 self.data[i].append(0)
+
+    @staticmethod
+    def fromArray(arr: list):
+        '''Create a single column matrix from an array.'''
+        m = Matrix(len(arr), 1)
+        for i in range(len(arr)):
+            m.data[i][0] = arr[i]
+        return m
+   
+    def toArray(self):
+        '''Create an array of a matrix.'''
+        arr = []
+        for i in range(self.rows):
+            for j in range(self.cols):
+                arr.append(self.data[i][j])
+        return arr
+
 
     # Print the matrix array
     def print(self):
-        """Print the matrix in a table form"""
+        '''Print the matrix in a table form'''
     
         print("")
         print(f"Matrix ({self.rows} rows x {self.cols} columns)")
@@ -68,23 +85,39 @@ class Matrix:
                     self.data[i][j] += n 
     
     # Scalar Substract
-    def substract(self, n):
+    # def substract(self, n):
+    #     '''
+    #     Substract:
+    #     ---
+    #         Substracts each matrix container with the value n.
+    #         Substract multiple matrixes from each other.
+    #     '''
+    #     for i in range(self.rows):
+    #         for j in range(self.cols):
+    #             if (isinstance(n, Matrix)):
+    #                 self.data[i][j] -= n.data[i][j]
+    #             else:
+    #                 self.data[i][j] -= n 
+
+    @staticmethod
+    def subtract(a, b):
         '''
-        Substract:
+        Subtract:
         ---
-            Substracts each matrix container with the value n.
-            Substract multiple matrixes from each other.
-        '''
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if (isinstance(n, Matrix)):
-                    self.data[i][j] -= n.data[i][j]
-                else:
-                    self.data[i][j] -= n 
+            Subtracts one matrix from another
+        '''   
+        if (isinstance(a, Matrix) and isinstance(b, Matrix)):
+            # TODO: Check matrixes size
+            result = Matrix(a.rows, a.cols)
+
+            for i in range(result.rows):
+                for j in range(result.cols):
+                    result.data[i][j] = a.data[i][j] - b.data[i][j]
+            return result
     
     # Multiply (Static Matrix)
     @staticmethod
-    def multiply(a: Matrix, b: Matrix) -> Matrix:
+    def multiply(a, b):
         '''
         Calculate the matrix product of two matrixes.
 
@@ -99,6 +132,7 @@ class Matrix:
         '''
         if (isinstance(a, Matrix) and isinstance(b, Matrix)):
             # Matrix Product
+            # TODO: Rewrite code of checking matrix size
             if (not a.cols == b.rows):
                 try:
                     raise Warning(f"The columns of the matrix ({a.cols}) don't match with the rows of the other matrix ({b.rows}).")
@@ -116,7 +150,7 @@ class Matrix:
                 return result
     
     # Multiply (Non Static Scalar)
-    def scalar(self, n):
+    def s_multiply(self, n):
         '''
         Scalar Multiply:
         ---
@@ -138,7 +172,7 @@ class Matrix:
                 result.data[j][i] = self.data[i][j]
         return result
 
-    # Map function
+    # Non Static Map function
     def map(self, fn):
         '''
         Executes function fn to all matrix container values individually and returns the values back in the matrix.
@@ -152,3 +186,21 @@ class Matrix:
             for j in range(self.cols):
                 val = self.data[i][j]
                 self.data[i][j] = fn(val)
+
+    @staticmethod
+    def s_map(matrix, fn):
+        '''
+        Executes function fn to all matrix container values individually and returns the values back in the matrix.
+       
+        Updated values:
+        ---
+            val = self.data[i][j]
+            self.data[i][j] = fn(val)
+        '''
+        for i in range(matrix.rows):
+            for j in range(matrix.cols):
+                val = matrix.data[i][j]
+                matrix.data[i][j] = fn(val)
+        
+        return matrix
+
--- nn.py
+++ nn.py
@@ -1,4 +1,12 @@
 from matrix import Matrix
+import math
+
+def sigmoid(x):
+    return 1  / (1 + math.exp(-x))
+
+def dsigmoid(y):
+    # return sigmoid(x) * (1 + math.exp(sigmoid(-x)))
+    return y * (1 - y)
 
 class NeuralNetwork:
     # Initialization
@@ -14,3 +22,89 @@ class NeuralNetwork:
 
         self.bias_h = Matrix(self.hidden_nodes, 1)
         self.bias_o = Matrix(self.hidden_nodes, 1)
+        self.bias_h.randomize()
+        self.bias_o.randomize()
+        self.learning_rate = 0.1
+
+    def feedforward(self, input_array: list):
+        # Generate input outputs
+        inputs = Matrix.fromArray(input_array)
+        hidden = Matrix.multiply(self.weights_ih, inputs)
+        hidden.add(self.bias_h)
+        hidden.map(sigmoid)
+
+        # Generate hidden outputs
+        outputs = Matrix.multiply(self.weights_ho, hidden)
+        outputs.add(self.bias_o)
+        outputs.map(sigmoid)
+
+        # Return outputs to call
+        return outputs.toArray()
+
+    def train(self, inputs_array: list, targets_array: list):
+        # Generate input outputs
+        targets = Matrix.fromArray(targets_array)
+        inputs = Matrix.fromArray(inputs_array)
+        hidden = Matrix.multiply(self.weights_ih, inputs) 
+        hidden.add(self.bias_h)
+        hidden.map(sigmoid)
+
+        # Generate hidden outputs
+        outputs = Matrix.multiply(self.weights_ho, hidden)
+        outputs.add(self.bias_o)
+        outputs.map(sigmoid)
+
+       
+        # Calculate the output errors
+        output_errors = Matrix.subtract(targets, outputs)
+
+        # Calculate Gradient
+        gradients = Matrix.s_map(outputs, dsigmoid)
+        gradients = Matrix.multiply(gradients, output_errors)
+        gradients.s_multiply(self.learning_rate)
+       
+        # Calculate Delta's
+        transposed_hidden = Matrix.transpose(hidden)
+        weight_ho_deltas = Matrix.multiply(gradients, transposed_hidden)
+        
+        # Adjust weights (hidden->outputs) by delta's
+        self.weights_ho.add(weight_ho_deltas)
+        # Adjust bias by it's delta's (gradients)
+        self.bias_o.add(gradients)
+        
+        # Calculate hidden layer errors
+        transposed_w_ho = Matrix.transpose(self.weights_ho)
+        hidden_errors = Matrix.subtract(transposed_w_ho, output_errors)
+
+        # Calculate hidden gradient
+        hidden_gradient = Matrix.s_map(hidden, dsigmoid)
+        hidden_gradient.multiply(hidden_errors)
+        hidden_gradient.multiply(self.learning_rate)
+
+        # Calculate inputs->hidden Delta's
+        transposed_inputs = Matrix.transpose(inputs)
+        weight_ih_deltas = Matrix.multiply(hidden_gradient, transposed_inputs)
+        
+        # Adjust weights (inputs->hidden) by delta's
+        self.weights_ih.add(weight_ih_deltas)
+        # Adjust bias by it's delta's (gradients)
+        self.bias_h.add(hidden_gradient)
+
+
+class Tdata:
+    def __init__(self, input_array, targets_array):
+        self.inputs = input_array
+        self.targets = targets_array
+
+a = [
+    Tdata([1,0],[1]),
+    Tdata([0,1],[1]),
+    Tdata([1,1],[0]),
+    Tdata([0,0],[0]),
+    ]
+
+nn = NeuralNetwork(2, 2, 1)
+
+for i in a:
+    nn.train(i.inputs, i.targets)
+
